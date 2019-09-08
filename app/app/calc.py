10863292516287c8890177574fade0cbaa69bea5
Unknown <miguel.molledo.alvarez@gmail.com>
2019-09-08 20:39:42 +0200

    Added django features
    Created users, modified base classes and executed migrations

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+
+
+
+def add(x, y):
+    """
+    
+    :param x: 
+    :param y: 
+    :return: 
+    """
+    return x + y
+
+
+def substract(x,y ):
+    return y-x
--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -62,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
         },
     },
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'core.User'
+
